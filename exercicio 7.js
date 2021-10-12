3e5e1694ea899e64e8d90ceefdc147db7a504e44
julioknm <jkbittencourtt@gmail.com>
2021-10-12 13:30:22 -0300

    Comentários

--- exercicio 7.js
+++ exercicio 7.js
@@ -1,10 +1,11 @@
-//A	 partir	 do	 ano	 fornecido	 pelo	 usuário	 escrever	 um	 programa	 que	 diga	 se	 o	 ano	 é bissexto
+//A partir do ano fornecido pelo usuário escrever um programa que diga se o ano é bissexto
 
 //Para um ano ser bissexto, ele precisa
 //1. Ser divisível por 4
-//2. Se divisível por 100, deve ser por 400
+//2. Se for divisível por 100, deve ser divisível por 400
 
 console.log("ANO BISSEXTO - Avalia se um ano é bissexto. \nPara sair, digite qualquer letra.\n");
+
 while (true){ 
   var ano = parseInt(prompt("Ano: "));
   if (!Number.isInteger(ano)){
@@ -18,16 +19,15 @@ while (true){
 
 
 function avaliaAno(ano){
-
-if (ano%4 == 0){
-  if ((ano % 100 == 0) && (ano % 400 != 0)){
-    console.log("Não é bissexto.\n");
+  if (ano%4 == 0){
+    if ((ano % 100 == 0) && (ano % 400 != 0)){
+      console.log("Não é bissexto.\n");
+    }
+    else {
+      console.log("É bissexto.\n");
+    }
   }
   else {
-    console.log("É bissexto.\n");
+    console.log("Não é bissexto.\n");
   }
-}
-else {
-  console.log("Não é bissexto.\n");
-}
 }
